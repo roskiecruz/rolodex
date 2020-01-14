b258a6e798c7195defffaa8d2aac47e831bf2dd9
roskiecruz <roskiecruz@gmail.com>
2020-01-14 02:23:36 -0800

    Updates

---
+++ precache-manifest.39a1d49b35ca0453fd69c19c8dc52190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f737d055523817200da9eaf0e6de065",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "37832c896b1ac97e6530",
+    "url": "/rolodex/static/css/main.82fbc901.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37832c896b1ac97e6530",
+    "url": "/rolodex/static/js/main.db857bfb.chunk.js"
+  },
+  {
+    "revision": "d72009913ef41833d3a3",
+    "url": "/rolodex/static/js/runtime-main.2fc8d5fe.js"
+  }
+]);
